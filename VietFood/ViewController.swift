52e24cca941743ad0325ba057af26461d6d57d84
HieuTDT <hieutdt@vng.com.vn>
2020-08-30 08:25:59 +0700

    Initial Commit

---
+++ VietFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VietFood
+//
+//  Created by HieuTDT on 8/30/20.
+//  Copyright © 2020 HieuTDT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
